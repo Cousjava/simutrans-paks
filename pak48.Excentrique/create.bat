f412a80005cc0bd2df478b1eda6e645ba5f56f61
h_malthaner <h_malthaner@e96eaf88-093e-0410-925f-862d53288611>
2011-12-17 23:31:20 +0000

    pak48.Excentrique configuration, translation, build file and license.

---
+++ pak48.Excentrique/create.bat
@@ -0,0 +1,58 @@
+del ..\simutrans\pak48.Excentrique\*.pak
+
+
+cd goods
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd ground
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd powerline
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd road
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd road_vehicles
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd rail
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd tree
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd decoration
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd city
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd pedestrians
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd misc
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd skin
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+cd menus
+call create.bat
+move *.pak ..\..\simutrans\pak48.Excentrique
+cd ..
+copy /y config\*.* ..\simutrans\pak48.Excentrique\config
+copy /y text\*.* ..\simutrans\pak48.Excentrique\text
+pause
